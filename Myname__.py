c5ad9164da3d4185acb7840e9384aff76ae9a8b9
Viiinnyyyyy <vincekllymarlinga@gmail.com>
2026-08-21 23:20:03 +0800

    Add files via upload

---
+++ Myname__.py
@@ -0,0 +1,2 @@
+code = "\t\t\t\t\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t*\t\t\t\tHI MY NAME IS\t\t\t\t\t*\n\t\t\t\t\t\t\t\t*\t\t\t\"Vince Marlinga\"\t\t\t\t*\n\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t\t\t\t\t\t\t*"
+print (code)
